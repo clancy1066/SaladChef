5714ce950010e447b1762533b705f2c8fecc550a
Clancy <clancy1066@yahoo.com>
2020-11-21 08:49:49 -0800

    Fixed a simple thing in the scorekeeper

--- Assets/Scripts/UI/PlayerScoreUI.cs
+++ Assets/Scripts/UI/PlayerScoreUI.cs
@@ -15,11 +15,13 @@ public class PlayerScoreUI : MonoBehaviour
 
         tmp = transform.Find("Score");
 
-        if (tmp != null) m_scoreText = tmp.gameObject.GetComponent<Text>();
+        if (tmp != null) 
+            m_scoreText = tmp.gameObject.GetComponent<Text>();
 
         tmp = transform.Find("Timer");
 
-        if (tmp != null) m_timerText = tmp.gameObject.GetComponent<Text>();
+        if (tmp != null) 
+            m_timerText = tmp.gameObject.GetComponent<Text>();
 
         SetScore(0);
         SetTime(0);
@@ -33,7 +35,11 @@ public class PlayerScoreUI : MonoBehaviour
 
     public void SetScore(uint score)
     {
-        if (m_scoreText != null) m_scoreText.text = score.ToString();
+        if (m_scoreText != null)
+        {
+            m_scoreText.color = Color.green;
+            m_scoreText.text = score.ToString();
+        }
     }
 
     public void SetTime(float time)
@@ -41,7 +47,7 @@ public class PlayerScoreUI : MonoBehaviour
         int minutes = (int)((time + 0.5f) / 60.0f);
         int seconds = (int)time % 60;
 
-        if (m_scoreText != null) m_scoreText.text = (minutes.ToString() +":" + seconds.ToString());
+        if (m_timerText != null) m_timerText.text = (minutes.ToString() +":" + seconds.ToString());
     }
 }
     
